57c8b34a3667740e1c359a6c57e9893ee365e4dd
miguelmpp <miguelmpp12@gmail.com>
2025-09-23 21:17:01 -0300

    adicionando projeto

---
+++ ObjetvoAtividade/Carro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Carro
+    {
+        public string Chassis { get; set; }
+        public string LicensePlate { get; set; }
+        public string Cor { get; set; }
+        public string Motor { get; set; }
+
+        public Carro(string chassis, string licensePlate, string cor, string motor)
+        {
+            Chassis = chassis;
+            LicensePlate = licensePlate;
+            Cor = cor;
+            Motor = motor;
+        }
+
+        public void Ligar() => Console.WriteLine("O carro esta ligado vrum vrum!");
+
+        public override string ToString()
+            => $"Detalhes do carro: Chassis: {Chassis}  Placa: {LicensePlate}  Cor: {Cor}  Motor: {Motor}";
+    }
+}
---
+++ ObjetvoAtividade/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro[] carList = new Carro[]
+            {
+                new Carro("Sedan",      "ABC1A23", "Vermelho",   "V6"),
+                new Carro("SUV",        "XYZ4B56", "Azul",       "Elétrico"),
+                new Carro("Hatchback",  "JKL7C89", "Preto",      "1.0 Turbo"),
+                new Carro("Pickup",     "MNO0D12", "Branco",     "V8"),
+                new Carro("Coupé",      "PQR3E45", "Prata",      "V6 Biturbo"),
+                new Carro("Conversível","STU6F78", "Amarelo",    "2.0"),
+                new Carro("Minivan",    "VWX9G01", "Verde",      "V6 Híbrido"),
+                new Carro("Sedan",      "AAA1H11", "Bordô",      "1.6"),
+                new Carro("SUV",        "BBB2I22", "Grafite",    "Híbrido"),
+                new Carro("Hatchback",  "CCC3J33", "Azul-claro", "Elétrico")
+            };
+
+            foreach (var carro in carList)
+                Console.WriteLine(carro);
+
+            Console.WriteLine();
+            carList[0].Ligar();
+        }
+    }
+}
